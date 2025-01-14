c223c6096c8a53d8efe0c9d608ed1581f883111a
Bahman026 <Bahman026@gmail.com>
2025-01-14 16:54:05 +0330

    feat(cli): add script for testing CLI functionalities

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+use DigitsSequence\Script;
+Script::run($argc, $argv);
---
+++ src/Script.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace DigitsSequence;
+
+class Script
+{
+    public static function run($argc, $argv): void
+    {
+        if ($argc < 2 || !is_numeric($argv[1]) || $argv[1] < 0) {
+            die("Usage: php script.php <n> [--full-number]\nWhere n is a non-negative integer.\n");
+        }
+        $n = intval($argv[1]);
+        $sequenceNumber = DigitsSequence::digitsSequence($n);
+        echo "($n) = " . $sequenceNumber;
+    }
+}
